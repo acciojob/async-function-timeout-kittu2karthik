3f2c12dbae7c93f5ad73c3bd7b2756c5d435bfb2
kittu2karthik <71444538+kittu2karthik@users.noreply.github.com>
2025-11-06 14:19:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-//your JS code here. If required.
+const btnEl = document.querySelector('#btn');
+
+btnEl.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const inputValue = document.querySelector('#text').value;
+  const delay = parseInt(inputValue.split(' - ')[1]); 
+
+  const output = document.querySelector('#output');
+
+  if (isNaN(delay)) {
+    output.textContent = 'Invalid input!';
+    return;
+  }
+
+  setTimeout(() => {
+    const p = document.createElement('p');
+    p.textContent = `Test - ${delay}`;
+    output.appendChild(p);
+  }, delay * 1000);
+});
